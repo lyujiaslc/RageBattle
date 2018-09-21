f0ffbf6b0ee4d55f60231d79e12457e2d5d87c48
JackyZhong <Jacky.Zhong@superluckycasino.com>
2018-09-21 16:59:56 -0700

    JZ- enhance environment

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -22,8 +22,8 @@ public class CameraManager : MonoBehaviour {
         }
         transform.position = (player1.position + player2.position) / 2;
         float dis = Vector3.Distance(player1.position, player2.position);
-        float size = dis;
-        m_camera.orthographicSize = Mathf.Clamp(Mathf.Lerp(m_camera.orthographicSize, size, 0.2f),9.0f, 22.0f);
+        float size = dis * 2;
+        m_camera.fieldOfView = Mathf.Clamp(Mathf.Lerp(m_camera.fieldOfView, size, 0.2f), 21f, 80.0f);
 
     }
 }
