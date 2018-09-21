ea39d7efb0bb05ada70f16cf79bf576d2fdfee26
lyujiaslc <jiayu.liu@superluckycasino.com>
2018-09-21 12:33:31 -0700

    added player control and managed game start

--- Assets/RageBattleGameManager.cs
+++ Assets/RageBattleGameManager.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class RageBattleGameManager : MonoBehaviour {
 
     [SerializeField] private GameObject playerPrefab = null;
-    [SerializeField] private Transform[] spawnPoints = null;
+
+    private RageBattlePlayer player1;
+    private RageBattlePlayer player2;
 
     public void InitializePlayers()
     {
-        Instantiate(playerPrefab, spawnPoints[0]);
-        Instantiate(playerPrefab, spawnPoints[1]);
+        player1 = Instantiate(playerPrefab, GetSpawnpoint(), Quaternion.identity).GetComponent<RageBattlePlayer>();
+        player1.playerIndex = 1;
+
+        player2 = Instantiate(playerPrefab, GetSpawnpoint(), Quaternion.identity).GetComponent<RageBattlePlayer>();
+        player2.playerIndex = 2;
+    }
+
+    public void OnGameStarted()
+    {
+        InitializePlayers();
     }
 
     public void OnGameFinished()
     {
-        
+        Destroy(player1.gameObject);
+        Destroy(player2.gameObject);
     }
 
-    private void SetupSpawnpoints(Transform[] spawnPoints)
+    private void Start()
     {
-        
+        OnGameStarted();
+    }
+
+    private Vector3 GetSpawnpoint()
+    {
+        return new Vector3(Random.Range(-20, 20), Random.Range(2, 8), Random.Range(-20, 20));
     }
 }
--- Assets/RageBattlePlayer.cs
+++ Assets/RageBattlePlayer.cs
@@ -4,10 +4,52 @@ using UnityEngine;
 
 public class RageBattlePlayer : MonoBehaviour {
 
-    public int playerIndex;
+    [HideInInspector]
+    public int playerIndex = 0;
 
-    private void Start()
+    [SerializeField] private float speed = 10;
+
+    private void Update()
     {
-        
+        // Player1 control
+        if(playerIndex == 1)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(transform.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-transform.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-transform.right * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(transform.right * Time.deltaTime * speed);
+            }
+        }
+        // Player2 control
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(transform.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-transform.forward * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-transform.right * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(transform.right * Time.deltaTime * speed);
+            }
+        }
     }
 }
