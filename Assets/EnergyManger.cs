663304fe7a798e398eb383e7db9b3bfd052f5a15
JackyZhong <Jacky.Zhong@superluckycasino.com>
2018-09-21 12:54:42 -0700

    JZ - Energy manager

---
+++ Assets/EnergyManger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyManger {
+
+    public enum EnergyType
+    {
+        HAPPY,
+        ANGRY
+    }
+
+
+
+    private List<Energy> energies = new List<Energy>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
+
