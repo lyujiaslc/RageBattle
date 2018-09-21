1242ea0949cd004ed74356acd1488d24cb3f8e1e
Jiayu Liu <jiayu.liu@superluckycasino.com>
2018-09-21 11:49:39 -0700

    set up field and player prefabs

---
+++ Assets/RageBattleGameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageBattleGameManager : MonoBehaviour {
+
+    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private Transform[] spawnPoints = null;
+
+    public void InitializePlayers()
+    {
+        Instantiate(playerPrefab, spawnPoints[0]);
+        Instantiate(playerPrefab, spawnPoints[1]);
+    }
+
+    public void OnGameFinished()
+    {
+        
+    }
+
+    private void SetupSpawnpoints(Transform[] spawnPoints)
+    {
+        
+    }
+}
---
+++ Assets/RageBattlePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageBattlePlayer : MonoBehaviour {
+
+    public int playerIndex;
+
+    private void Start()
+    {
+        
+    }
+}
